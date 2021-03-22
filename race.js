b74fca22f07fa045f7cadb9caa77e92969486e3b
anaguzun <ana.busuioc@yahoo.com>
2021-03-22 17:47:37 +0100

    modificari la else if

--- race.js
+++ race.js
@@ -1,16 +1,18 @@
 let raceNumber = Math.floor(Math.random() * 1000 + 1);
-let earlyRegistration = true;
+let earlyRegistration = true ;
 let runnerAge = 18 ;
 
 if (earlyRegistration) {
 raceNumber += 1000;}
 
-if(runnerAge >= 18 && earlyRegistration == true && raceNumber >= 1000 ) {
+if(runnerAge >= 18 && earlyRegistration  && raceNumber >= 1000 ) {
 console.log(`Your race starts at 9:30am and yout race numaber is ${raceNumber}`);
-}else if(runnerAge >= 18 && earlyRegistration == false) {
+}else if(runnerAge >= 18 && !earlyRegistration) {
   console.log(`Your race starts at 11:00am and your race number is ${raceNumber}`);
-}else(runnerAge < 18 && earlyRegistration == false); {
+}else if(runnerAge < 18 && earlyRegistration) {
   console.log(`Your race starts at 12:30pm and your race number is ${raceNumber}`);
+}else{
+  console.log('Please see the desk registration for Information!');
 }
 
 
